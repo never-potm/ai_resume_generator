3b97c79b65d45d79c927ff53096e65691ed119f4
Suraj Gusain <suraj.gusain15@gmail.com>
2025-06-11 22:25:08 +0530

    added input component; added fields in resume form for step 1

--- components/resume/step-one.js
+++ components/resume/step-one.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import {Button} from '@/components/ui/button';
+import {Input} from '@/components/ui/input';
 
 function StepOne(props) {
     return (
-        <div>step 1</div>
+        <div className="w-full lg:w-1/2 p-5 shadow-lg border-t-4 rounded-lg">
+            <h2 className="text-2xl font-bold mb-5">Personal Information</h2>
+            <Input label="Name" className="mb-3"/>
+            <Input label="Name" className="mb-3"/>
+            <Input label="Name" className="mb-3"/>
+            <Input label="Name" className="mb-3"/>
+            <Input label="Name" className="mb-3"/>
+            <div className="flex justify-end">
+                <Button>Save</Button>
+            </div>
+        </div>
     );
 }
 
--- context/resume.js
+++ context/resume.js
@@ -14,7 +14,7 @@ const initialState = {
 export function ResumeProvider({children}) {
 
     const [resume, setResume] = React.useState(initialState);
-    const [step, setStep] = React.useState(2);
+    const [step, setStep] = React.useState(1);
 
     return <ResumeContext.Provider value={{step, setStep, resume, setResume}}>{children}</ResumeContext.Provider>;
 }
