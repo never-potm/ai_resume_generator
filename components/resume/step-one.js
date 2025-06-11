8af3889f7a2d2c6e62d8598c2d19acd20c6d328f
Suraj Gusain <suraj.gusain15@gmail.com>
2025-06-11 22:37:28 +0530

    added personal information fields in step 1 and added handleSubmit to show information

--- components/resume/step-one.js
+++ components/resume/step-one.js
@@ -1,19 +1,70 @@
 import React from 'react';
 import {Button} from '@/components/ui/button';
 import {Input} from '@/components/ui/input';
+import {useResume} from "@/context/resume";
 
-function StepOne(props) {
+function StepOne() {
+
+    const {resume, setResume} = useResume();
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); // prevents default page reload on submit behavior of javascript
+        console.log(resume);
+
+        // save to database
+
+        // goto next step
+    }
+
     return (
         <div className="w-full lg:w-1/2 p-5 shadow-lg border-t-4 rounded-lg">
             <h2 className="text-2xl font-bold mb-5">Personal Information</h2>
-            <Input label="Name" className="mb-3"/>
-            <Input label="Name" className="mb-3"/>
-            <Input label="Name" className="mb-3"/>
-            <Input label="Name" className="mb-3"/>
-            <Input label="Name" className="mb-3"/>
-            <div className="flex justify-end">
-                <Button>Save</Button>
-            </div>
+            <form onSubmit={handleSubmit}>
+                <Input
+                    type="text"
+                    className="mb-3"
+                    onChange={e => setResume({...resume, name: e.target.value})}
+                    value={resume.name}
+                    placeholder="Name"
+                    autoFocus
+                    required
+                />
+                <Input
+                    type="text"
+                    className="mb-3"
+                    onChange={e => setResume({...resume, address: e.target.value})}
+                    value={resume.address}
+                    placeholder="Address"
+                    required
+                />
+                <Input
+                    type="number"
+                    className="mb-3"
+                    onChange={e => setResume({...resume, phone: e.target.value})}
+                    value={resume.phone}
+                    placeholder="Phone number"
+                    required
+                />
+                <Input
+                    type="email"
+                    className="mb-3"
+                    onChange={e => setResume({...resume, email: e.target.value})}
+                    value={resume.email}
+                    placeholder="Email"
+                    required
+                />
+                <Input
+                    type="text"
+                    className="mb-3"
+                    onChange={e => setResume({...resume, job: e.target.value})}
+                    value={resume.job}
+                    placeholder="Job title"
+                    required
+                />
+                <div className="flex justify-end">
+                    <Button>Save</Button>
+                </div>
+            </form>
         </div>
     );
 }
