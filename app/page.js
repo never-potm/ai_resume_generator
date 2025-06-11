9c73490dfa94b5daeff4de1e0be07d7973ad7698
Suraj Gusain <suraj.gusain15@gmail.com>
2025-06-11 15:40:24 +0530

    added dark/light mode toggle

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import {Button} from '@/components/ui/button';
 import Link from 'next/link';
+import {ModeToggle} from "@/components/nav/mode-toggle";
 
 export default function Home() {
   return (
       <div className="flex justify-center items-center h-screen">
+          <ModeToggle />
           <Link href="/resume/create">
               <Button>start creating AI resume</Button>
           </Link>
