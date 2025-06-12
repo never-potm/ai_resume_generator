d858a5af351c341905e154b7df661b1bee7b3520
Suraj Gusain <suraj.gusain15@gmail.com>
2025-06-12 15:45:03 +0530

    added resume model for mongo

---
+++ models/resume.js
@@ -0,0 +1,40 @@
+import mongoose, {Schema, model} from "mongoose";
+
+const ExperienceSchema = new Schema({
+    title: String,
+    company: String,
+    address: String,
+    startDate: String,
+    endDate: String,
+    summary: String
+});
+
+const EducationSchema = new Schema({
+    name: String,
+    address: String,
+    qualification: String,
+    year: String
+});
+
+const SkillSchema = new Schema({
+    name: String,
+    level: String
+});
+
+const ResumeSchema = new Schema(
+    {
+        userEmail: {type: String, required: true},
+        title: String,
+        name: String,
+        job: String,
+        address: String,
+        themeColor: String,
+        summary: String,
+        experience: [ExperienceSchema],
+        education: [EducationSchema],
+        skills: [SkillSchema],
+    },
+    {timestamps: true}
+);
+
+export default Resume = mongoose.models.Resume || model("Resume", ResumeSchema);
