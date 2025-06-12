0a4186eff9d6b957880101afd56c55fb5b921899
Suraj Gusain <suraj.gusain15@gmail.com>
2025-06-12 16:21:56 +0530

    added a server action to save resume to mongodb

---
+++ actions/resume.js
@@ -0,0 +1,19 @@
+"use server";
+
+import db from '@/utils/db';
+import Resume from '@/models/resume';
+import {currentUser} from '@clerk/nextjs/server';
+
+export const saveResumeToDB = async (data) => {
+    try {
+        db()
+        const user = await currentUser();
+        const userEmail = user.emailAddresses[0]?.emailAddress;
+
+        const {_id, ...rest} = data;
+        const resume = await Resume.create({...rest, userEmail});
+        return JSON.parse(JSON.stringify(resume));
+    } catch (e) {
+        throw new Error(e);
+    }
+}
