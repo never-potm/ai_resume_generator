8d4b156d02b2cb723aea0c09c1380136dbd74a3f
Suraj Gusain <suraj.gusain15@gmail.com>
2025-06-13 18:16:21 +0530

    Added skeleton cards while the resume information gets loaded in dashboard page

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,9 +1,24 @@
 "use client";
 import React from 'react';
 import {useResume} from "@/context/resume";
+import SkeletonCard from "@/components/cards/skeleton-card";
 
 export default function Dashboard() {
     const {retrievedResumes} = useResume();
+
+    if (!retrievedResumes?.length) {
+        return (
+            <div>
+                <p className="text-center">Loading...</p>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 m-5 px-5">
+                    <SkeletonCard/>
+                    <SkeletonCard/>
+                    <SkeletonCard/>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <div>
             <pre>{JSON.stringify(retrievedResumes, null, 2)}</pre>
