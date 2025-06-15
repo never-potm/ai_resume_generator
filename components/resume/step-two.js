11141f5f9fb47ca1969df2682b0bb4f7cccd82c1
Suraj Gusain <suraj.gusain15@gmail.com>
2025-06-15 11:42:01 +0530

    added generateAI controls in summary page

--- components/resume/step-two.js
+++ components/resume/step-two.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Textarea} from "@/components/ui/textarea";
 import {useResume} from "@/context/resume";
 import {Button} from "@/components/ui/button";
+import {Brain, Loader2} from "lucide-react";
 
 function StepTwo() {
+    // context
     const {resume, setResume, updateResume, setStep} = useResume();
+
+    const [loading, setLoading] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -12,9 +16,27 @@ function StepTwo() {
         setStep(3);
     }
 
+    const handleGenerateWithAI = () => {
+        alert("calling AI to generate content")
+    }
+
     return (
         <div className="w-full p-5 shadow-lg border-t-4 rounded-lg">
-            <h2 className="text-2xl font-bold m-5">Summary</h2>
+            <div className="flex justify-between">
+                <h2 className="text-2xl font-bold mb-5" style={{color: resume.themeColor}}>Summary</h2>
+
+                <Button variant="destructive" onClick={handleGenerateWithAI} disabled={loading}>
+                    {loading ? (
+                            <Loader2 size={18} className="mr-2 animate-spin"/>
+                        )
+                        : (
+                            <Brain size={18} className="mr-2"/>
+                        )
+                    }
+                    Generate with AI
+                </Button>
+
+            </div>
 
             <Textarea
                 onChange={e => setResume({...resume, summary: e.target.value})}
